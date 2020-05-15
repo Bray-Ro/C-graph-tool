3081b4280e73458e411baf59001e0178804f6147
rohanThegreaTT <44115084+rohanThegreaTT@users.noreply.github.com>
2020-05-15 10:49:29 -0400

    Update and rename index.h to grpher.h

---
+++ grpher.h
@@ -0,0 +1,15 @@
+
+void create_graph(int int_data, char str_label[9000]) {
+  
+    printf("\n%s(%d):", str_label, int_data);
+    printf("\n");
+    int i = 0;
+    while (i < int_data) {
+        if (int_data > 71) {
+            printf("\n#");
+        } else {
+            printf("#");
+        }
+        i++;
+    }
+}
